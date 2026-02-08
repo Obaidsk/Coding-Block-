40c61b4a93643bba31f6b7fce9f8c1468c1e9927
Obaid21 <obaidsk215@gmail.com>
2026-02-09 00:29:23 +0530

    Add files via upload

---
+++ C++/Lecture 4 28-01-2026/Pattern12.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+ int  n=5;
+ int j;
+  for(int i = 1 ;i<=n;i++){
+     for(int j=1 ;j<=n-i;j++){
+		   cout<<" ";
+             }
+           for(int j =1 ;j<=i;j++){
+           	if(j%2!=0){
+           		cout<<"*";
+			   }else {
+			   
+		   cout<<"!";
+	}
+              }
+		      for(int j =2 ;j<=i;j++){
+		      	if((j+i)%2!=0){
+		      		cout<<"!";
+				  }
+		       else {
+		       	cout<<"*";
+			   }
+           }
+           cout<<endl;
+        }
+        
+        return 0;
+    }
+      
+      
+  
+
+ 
+
