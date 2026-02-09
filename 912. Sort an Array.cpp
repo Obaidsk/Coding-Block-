7572167d9fdfe099b39babbfb1bfbcd4fc63e8f3
Obaidsk <obaidsk215@gmail.com>
2026-02-09 18:50:53 +0530

    09-02-2026, 6:00 PM

---
+++ 912. Sort an Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[i] < nums[j]) {
+                    nums[i] = nums[i] + nums[j];
+                    nums[j] = nums[i] - nums[j];
+                    nums[i] = nums[i] - nums[j];
+                }
+            }
+        }
+        return nums;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {5, 2, 3, 1, 4};
+
+    vector<int> sorted = sol.sortArray(nums);
+
+    cout << "Sorted array: ";
+    for (int num : sorted) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
