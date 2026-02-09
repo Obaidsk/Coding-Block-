6391fa8569558ca19895226aa92c1105205e76de
Obaidsk <obaidsk215@gmail.com>
2026-02-09 18:53:22 +0530

    09-02-2026, 12:00 PM

--- 912. Sort an Array.cpp
+++ 912. Sort an Array.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
+            for (int j = 0; j< nums.size(); j++) {
                 if (nums[i] < nums[j]) {
                     nums[i] = nums[i] + nums[j];
                     nums[j] = nums[i] - nums[j];
