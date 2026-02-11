4054dbf45da47343c71ebbbffdeed23abf234595
Obaidsk <obaidsk215@gmail.com>
2026-02-11 11:53:24 +0530

    09-02-2026

--- C++/Lecture 9 09-02-2026/912. Sort an Array.cpp
+++ C++/Lecture 9 09-02-2026/912. Sort an Array.cpp
@@ -4,19 +4,42 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j< nums.size(); j++) {
-                if (nums[i] < nums[j]) {
-                    nums[i] = nums[i] + nums[j];
-                    nums[j] = nums[i] - nums[j];
-                    nums[i] = nums[i] - nums[j];
-                }
-            }
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low, right = mid+ + 1;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right])
+                temp.push_back(nums[left++]);
+            else
+                temp.push_back(nums[right++]);
         }
+
+        while (left <= mid)
+            temp.push_back(nums[left++]);
+
+        while (right <= high)
+            temp.push_back(nums[right++]);
+
+        for (int i = low; i <= high; i++)
+            nums[i] = temp[i - low];
+    }
+
+    void mergeSort(vector<int>& nums, int low, int high) {
+        if (low >= high) return;
+
+        int mid = (low + high) / 2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 };
+
 
 int main() {
     Solution sol;
